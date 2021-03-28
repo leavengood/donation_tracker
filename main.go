cbad0c4319a5a531f1d34e1474cfb9df0a564ac0
Ryan Leavengood <leavengood@gmail.com>
2021-03-27 23:48:39 -0400

    Improve command-line parameter handling with flags package
    Also removed commented out code.

--- main.go
+++ main.go
@@ -1,37 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 	"time"
 
 	"github.com/leavengood/donation_tracker/paypal"
 	"github.com/leavengood/donation_tracker/util"
 )
 
-// var month = flag.Int("month", 0, "Provide a month number for which you want to fetch fresh data")
-// var year = flag.Int("year", 0, "Provide a year for which you want to fetch fresh data")
-// var skipUpload = flag.Bool("skip-upload", false, "Skip uploading the donation summary to the CDN")
-
-const usage = `Usage: %s [command]
+const usage = `
+Usage: %s [command]
 
 If no command is given, the default command of "update" is performed for the
 current year.
 
 Commands:
-    update [year] [--skip-upload | -s]
+    update [-year int] [-skip-upload]
         Update the donation information for the given year, defaulting to the
         current year. Summary information is uploaded as JSON to the Haiku CDN
-        for the current year only unless the --skip-upload or -s flag are
-        provided.
+        for the current year only unless the --skip-upload flag is provided.
 
-    summarize [year]
+    summarize [-year int]
         Provide a summary of a given year, defaulting to the current year. No
         new data is downloaded.
 
-    fetch <month> [year]
+    fetch <-month int> [-year int]
         Fetch and save a single month of transactions from PayPal given a
         numeric month and optionally a year. The default year is the current
         year. Overwrites any existing data.
@@ -53,11 +49,34 @@ func main() {
 	}
 
 	// Default command is update
+	exe := os.Args[0]
 	cmd := "update"
-	args := os.Args
-	if len(args) > 1 {
-		cmd = args[1]
+	args := os.Args[1:]
+	if len(args) > 0 {
+		// This could be a flag for the default command of update
+		if args[0][0] != '-' {
+			cmd = args[0]
+			args = args[1:]
+		}
 	}
+
+	currentYear, currentMonth, _ := time.Now().UTC().Date()
+
+	flagSet := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	year := currentYear
+	flagSet.IntVar(&year, "year", currentYear, "Specifies the year to operate on")
+	month := int(currentMonth)
+	flagSet.IntVar(&month, "month", int(currentMonth), "Specifies the month to operate on")
+	skipUpload := false
+	flagSet.BoolVar(&skipUpload, "skip-upload", false, "Skip uploading data to the server on the 'update' command")
+
+	printUsage := func() {
+		fmt.Println(fmt.Sprintf(usage, exe))
+		fmt.Println("\nSupported flags:")
+		flagSet.PrintDefaults()
+	}
+	flagSet.Usage = printUsage
+	flagSet.Parse(args)
 
 	introPrint := func(msg string) {
 		fmt.Printf("%s %s...\n\n", util.Colorize(util.Green, "✷"), msg)
@@ -66,17 +85,10 @@ func main() {
 	greenCheck := util.Colorize(util.Green, "✓")
 	blueArrow := util.Colorize(util.Blue, "❯")
 
-	// currentYear, currentMonth, _ := time.Now().UTC().Date()
-	currentYear, currentMonth, _ := time.Now().UTC().Date()
-
-	// isCurrentYear := *year == 0 || *year == currentYear
-	// fmt.Printf("Is current year: %v\n", isCurrentYear)
-
 	getExchangeRate := func() float32 {
 		fmt.Print("Fetching exchange rate for EUR to USD...")
 		rate, err := GetExchangeRate(config.FixerIoAccessKey)
 		if err != nil {
-			// FIXME: Provide a helper for error printing and exit
 			exit(fmt.Sprintf("Error: could not get EUR to USD exchange rate: %v", err), 1)
 		}
 		if rate == float32(0) {
@@ -86,60 +98,26 @@ func main() {
 		return rate
 	}
 
-	getYear := func(yearStr string) int {
-		year, err := strconv.Atoi(yearStr)
-		if err != nil {
-			exit(fmt.Sprintf("Error: invalid year provided: %s", yearStr), 1)
-		}
-		if year < 2010 || year > currentYear {
-			exit(fmt.Sprintf("Error: Please provide a year between 2010 and %d", currentYear), 1)
-		}
-
-		return year
+	// Sanity check the year
+	if year < 2010 || year > currentYear {
+		exit(fmt.Sprintf("Error: Please provide a year between 2010 and %d", currentYear), 1)
 	}
-
-	getMonth := func(monthStr string, year int) int {
-		month, err := strconv.Atoi(monthStr)
-		if err != nil {
-			exit(fmt.Sprintf("Error: invalid month provided: %s", monthStr), 1)
-		}
-		maxMonth := 12
-		if year == currentYear {
-			maxMonth = int(currentMonth)
-		}
-		if month < 1 || month > maxMonth {
-			exit(fmt.Sprintf("Error: Please provide a month between 1 and %d", maxMonth), 1)
-		}
-
-		return month
-	}
-
-	year := currentYear
 
 	util.PrintLogo()
 
 	client := paypal.NewClient(config.PayPal)
 
 	switch cmd {
-	case "help", "-help", "--help", "-h":
-		exit(fmt.Sprintf(usage, args[0]), 0)
+	case "help":
+		printUsage()
+		os.Exit(0)
 
 	case "update":
-		skipUpload := false
+		fmt.Printf("Would update with year %d, month %d and skip-upload: %t\n", year, month, skipUpload)
+
 		extraMsg := ""
-
-		if len(args) == 3 {
-			year = getYear(args[2])
-		}
-
-		if len(args) == 4 {
-			flag := args[3]
-			if flag == "--skip-upload" || flag == "-s" {
-				skipUpload = true
-				extraMsg = ", skipping upload of data."
-			} else {
-				fmt.Printf("Warning: unknown flag passed to update: %s\n", flag)
-			}
+		if skipUpload {
+			extraMsg = ", skipping upload of data."
 		}
 
 		introPrint(fmt.Sprintf("Updating donation information for %d%s", year, extraMsg))
@@ -166,10 +144,6 @@ func main() {
 		fmt.Printf("\n%s Update complete!\n", greenCheck)
 
 	case "summarize":
-		if len(args) == 3 {
-			year = getYear(args[2])
-		}
-
 		fm, err := paypal.NewFileManager(year)
 		if err != nil {
 			exit(fmt.Sprintf("Error: could not load PayPal files for year %d: %v\n", year, err), 1)
@@ -184,15 +158,14 @@ func main() {
 		SummarizeYear(year, getExchangeRate(), fm)
 
 	case "fetch":
-		if len(args) < 3 {
-			exit("Error: please provide a month to fetch", 1)
+		// Sanity check the month
+		maxMonth := 12
+		if year == currentYear {
+			maxMonth = int(currentMonth)
 		}
-
-		if len(args) == 4 {
-			year = getYear(args[3])
+		if month < 1 || month > maxMonth {
+			exit(fmt.Sprintf("Error: Please provide a month between 1 and %d", maxMonth), 1)
 		}
-
-		month := getMonth(args[2], year)
 
 		fm := paypal.NewEmptyFileManager(year)
 		if err := client.GetAndSaveMonth(year, month, fm); err != nil {
@@ -200,10 +173,6 @@ func main() {
 		}
 
 	case "personal":
-		if len(args) == 3 {
-			year = getYear(args[2])
-		}
-
 		fm, err := paypal.NewFileManager(year)
 		if err != nil {
 			exit(fmt.Sprintf("Error: could not load PayPal files for year %d: %v\n", year, err), 1)
@@ -241,103 +210,4 @@ func main() {
 		fmt.Printf("Error: Unknown command %s.\n\n", cmd)
 		exit(fmt.Sprintf(usage, args[0]), 1)
 	}
-
-	// TODO: Extract this into a function
-	// if *month != 0 {
-	// 	if *year == 0 {
-	// 		*year = currentYear
-	// 	}
-
-	// 	if *year == currentYear && (*month < 1 || *month > int(currentMonth)) {
-	// 		fmt.Printf("Please choose a month between 1 and %d\n", currentMonth)
-	// 		os.Exit(1)
-	// 	}
-
-	// 	fmt.Printf("You chose month %d and year %d\n", *month, *year)
-	// 	ppts := FetchPayPalTxnsForMonth(*year, *month)
-
-	// 	fmt.Printf("There are %v transactions:\n", len(ppts))
-	// 	for _, ppt := range ppts {
-	// 		fmt.Println(ppt)
-	// 	}
-
-	// 	// Save to file
-	// 	filename := payPalTxnsFileName(*year, *month)
-	// 	fmt.Printf("Attempting to save %d transactions to file %s\n", len(ppts), filename)
-	// 	err := savePayPalTxnsToFile(filename, ppts)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-
-	// 	donations, other := ppts.FilterDonations()
-	// 	fmt.Printf("\n\nThere are %v donations:\n", len(donations))
-	// 	for _, ppt := range donations {
-	// 		fmt.Println(ppt)
-	// 	}
-	// 	fmt.Printf("\n\nThere are %v other transactions:\n", len(other))
-	// 	for _, ppt := range other {
-	// 		fmt.Println(ppt)
-	// 	}
-
-	// cc := donations.TotalByCurrency()
-	// for currency, amt := range cc {
-	// 	fmt.Printf("Total for %s: %.02f\n", currency, amt)
-	// }
-
-	// eurToUsdRate, err := GetExchangeRate()
-	// //eurToUsdRate := float32(1.2436)
-	// if err == nil {
-	// 	fmt.Printf("Grand Total (at EUR to USD rate of %f): %.02f\n", eurToUsdRate, cc.GrandTotal(eurToUsdRate))
-	// }
-
-	// 	currentSummaries := donations.Summarize()
-	// 	for month, summary := range currentSummaries {
-	// 		fmt.Printf("%s: %v\n", month, summary)
-	// 		fmt.Printf("Gross Total: %v\n", summary.GrossTotal())
-	// 		fmt.Printf("Net Total: %v\n\n", summary.NetTotal())
-	// 		b, _ := json.Marshal(summary)
-	// 		fmt.Println(string(b))
-	// 	}
-	// 	os.Exit(0)
-	// }
-	// fmt.Println("Did not get a month")
-	// summaryProcess()
-	// fm, err := NewPayPalFileManager(2019)
-	// // months, err := loadPayPalFiles("data", 2019)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Printf("Found %d months worth of files\n", len(fm.Months))
-	// for month, txns := range fm.Months {
-	// 	fmt.Printf("There are %d transactions for month %d\n", len(txns), month)
-	// }
-	// fmt.Printf("The most recent month is: %d\n", fm.GetLatestMonth())
-	// fmt.Printf("The missing months are: %v\n", fm.GetMissingMonths())
-
-	// Start with this so we fail fast if it has an error
-	// eurToUsdRate, err := GetExchangeRate(config.FixerIoAccessKey)
-	// if err != nil {
-	// 	log.Fatalf("could not get EUR to USD exchange rate because of error: %v\n", err)
-	// }
-	// if eurToUsdRate == float32(0) {
-	// 	log.Fatalln("we got a 0 exchange rate from fixer.io, is the access key correct?")
-	// }
-
-	// ds, err := ProcessYear(currentYear, eurToUsdRate)
-	// if err != nil {
-	// 	log.Fatalf("could not process year %d due to error: %v\n", currentYear, err)
-	// }
-
-	// fmt.Printf("Donation Summary: %#v\n", ds)
-
-	// // Update the JSON file, if this is the current year and the skip flag was not set
-	// if isCurrentYear && !*skipUpload {
-	// 	fmt.Println("Uploading donation summary...")
-	// 	err = UploadJson(ds)
-	// 	if err != nil {
-	// 		log.Fatalln(err)
-	// 	}
-	// } else {
-	// 	fmt.Println("Skipping upload of donation summary")
-	// }
 }
